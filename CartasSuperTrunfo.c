80d9e96705f164879b2c9e4b0cb8fd64638c3880
Iksson <vitorvkb@gmail.com>
2026-03-19 14:26:57 -0300

    Reestruturação das variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,36 +1,20 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
   
-// Área para definição das variáveis para armazenar as propriedades das cidades
+  // Declaração das variáveis das cartas
+  char estado1, estado2;
+  char codigo1[4], codigo2[4];
+  char cidade1[50], ciddade2[50];
+  unsigned long long populacao1, populacao2;
+  float area1, area2;
+  unsigned long long pib1, pib2;
+  int pontosturisticos1, pontosturisticos2;
 
-  // Carta 1
-  char estado1;
-  char codigo1[4];
-  char cidade1[50];
-  int populacao1;
-  float area1;
-  float pib1;
-  int pontosturisticos1;
-
-  //Carta 2
-  char estado2;
-  char codigo2[4];
-  char cidade2[50];
-  int populacao2;
-  float area2;
-  float pib2;
-  int pontosturisticos2;
-
-  // Adição de variáveis para densidade populacional e PIB per capita - Nível Aventureiro
-  float densidade1;
-  float densidade2;
-  float pib_per_capita1;
-  float pib_per_capita2;
+  // Variáveis calculadas
+  float densidade1, densidade2;
+  double pib_per_capita1, pib_per_capita2;
+  float superpoder1, superpoder2;
   
 // Área para entrada de dados
   
