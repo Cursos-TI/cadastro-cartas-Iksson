efb3cf194d4624aa04326d9905988681449c30e9
Iksson <vitorvkb@gmail.com>
2026-03-19 14:46:56 -0300

    Comparação de atributos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,7 +13,7 @@ int main() {
 
   // Variáveis calculadas
   float densidade1, densidade2;
-  double pib_per_capita1, pib_per_capita2;
+  double pibcapita1, pibcapita2;
   float superpoder1, superpoder2;
   
   // Entrada de dados
@@ -39,12 +39,21 @@ int main() {
   densidade1 = (float)populacao1 / area1;
   densidade2 = (float)populacao2 / area2;
 
-  pib_per_capita1 = (double)pib1 * 1e9 / populacao1;
-  pib_per_capita2 = (double)pib2 * 1e9 / populacao2;
+  pibcapita1 = (double)pib1 * 1e9 / populacao1;
+  pibcapita2 = (double)pib2 * 1e9 / populacao2;
 
-  superpoder1 = (float)populacao1 + area1 + (float)pib1 * 1e9f + pontosturisticos1 + (float)pib_per_capita1 + (1.0f / densidade1);
-  superpoder2 = (float)populacao2 + area2 + (float)pib2 * 1e9f + pontosturisticos2 + (float)pib_per_capita2 + (1.0f / densidade2);
+  superpoder1 = (float)populacao1 + area1 + (float)pib1 * 1e9f + pontosturisticos1 + (float)pibcapita1 + (1.0f / densidade1);
+  superpoder2 = (float)populacao2 + area2 + (float)pib2 * 1e9f + pontosturisticos2 + (float)pibcapita2 + (1.0f / densidade2);
 
+  // Comparações (1 = Carta 1 vence, 0 = Carta 2 vence)
+  int pop_venceu = populacao1 > populacao2;
+  int area_venceu = area1 > area2;
+  int pib_venceu = pib1 > pib2;
+  int pontos_venceu = pontosturisticos1 > pontosturisticos2;
+  int densidade_venceu = densidade1 < densidade2; // menor densidade vence
+  int pibcapita_venceu = pibcapita1 > pibcapita2;
+  int superpoder_venceu = superpoder1 > superpoder2;
+  
   printf("\n===== CARTA 1 =====\n");
   printf("Estado: %c\n", estado1);
   printf("Codigo: %s\n", codigo1);
